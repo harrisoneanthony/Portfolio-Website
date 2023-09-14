9031840c3086e26bc19d6e87c1109b0615e498be
Harrison Anthony <harrisoneanthony@gmail.com>
2023-09-14 12:20:23 -0400

    changed layout of footer to display space between

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,47 +12,50 @@ const Footer = () => {
 
     return (
         <div className="footer-container">
-            <div className="footer-head">
-                <h3>Website powered by</h3>
+            <div className="footer-l">
+                <h3 id="left-text">Website powered by:</h3>
                 <i className="devicon-react-original-wordmark colored react-icon"></i>
             </div>
-            <div className="contact-links">
-                <a
-                    className="links"
-                    href="mailto:harrisoneanthony@gmail.com"
-                    onMouseEnter={() => setGmailHovered(true)}
-                    onMouseLeave={() => setGmailHovered(false)}
-                >
-                    <img
-                        className={`icons ${isGmailHovered ? 'hovered' : ''}`}
-                        src={GmailIcon}
-                        alt="Gmail"
-                    />
-                </a>
-                <a
-                    className="links"
-                    href="https://github.com/harrisoneanthony"
-                    onMouseEnter={() => setGitHubHovered(true)}
-                    onMouseLeave={() => setGitHubHovered(false)}
-                >
-                    <img
-                        className={`icons ${isGitHubHovered ? 'hovered' : ''}`}
-                        src={GitHubIcon}
-                        alt="GitHub"
-                    />
-                </a>
-                <a
-                    className="links"
-                    href="https://www.linkedin.com/in/harrison-anthony-914763141/"
-                    onMouseEnter={() => setLinkedInHovered(true)}
-                    onMouseLeave={() => setLinkedInHovered(false)}
-                >
-                    <img
-                        className={`icons ${isLinkedInHovered ? 'hovered' : ''}`}
-                        src={LinkedInIcon}
-                        alt="LinkedIn"
-                    />
-                </a>
+            <div className="footer-r">
+                <h3>Follow me on:</h3>
+                <div className="contact-links">
+                    <a
+                        className="links"
+                        href="mailto:harrisoneanthony@gmail.com"
+                        onMouseEnter={() => setGmailHovered(true)}
+                        onMouseLeave={() => setGmailHovered(false)}
+                    >
+                        <img
+                            className={`icons ${isGmailHovered ? 'hovered' : ''}`}
+                            src={GmailIcon}
+                            alt="Gmail"
+                        />
+                    </a>
+                    <a
+                        className="links"
+                        href="https://github.com/harrisoneanthony"
+                        onMouseEnter={() => setGitHubHovered(true)}
+                        onMouseLeave={() => setGitHubHovered(false)}
+                    >
+                        <img
+                            className={`icons ${isGitHubHovered ? 'hovered' : ''}`}
+                            src={GitHubIcon}
+                            alt="GitHub"
+                        />
+                    </a>
+                    <a
+                        className="links"
+                        href="https://www.linkedin.com/in/harrison-anthony-914763141/"
+                        onMouseEnter={() => setLinkedInHovered(true)}
+                        onMouseLeave={() => setLinkedInHovered(false)}
+                    >
+                        <img
+                            className={`icons ${isLinkedInHovered ? 'hovered' : ''}`}
+                            src={LinkedInIcon}
+                            alt="LinkedIn"
+                        />
+                    </a>
+                </div>
             </div>
         </div>
     );
